eaf46917d062621dba007b3393b8524d72bf1f37
nahiantechx <nahian.easytechx@gmail.com>
2025-10-13 13:44:32 +0600

    Added tag manager in layout tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { CartProvider } from "@/context/CartContext";
 import ClientLayout from "@/components/ClientLayout";
 import { Lato } from 'next/font/google'
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import Script from "next/script";
 
 const lato = Lato({
   subsets: ['latin'], 
@@ -30,10 +31,36 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased lato.classname`}>
+      <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MT4Q8XHR');`
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </head>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased ${lato.className}`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MT4Q8XHR"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <CartProvider>
           <ClientLayout>{children}</ClientLayout>
-           <SpeedInsights />
+          <SpeedInsights />
         </CartProvider>
       </body>
     </html>
